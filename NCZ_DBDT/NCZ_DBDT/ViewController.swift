d8a55c5781634589a9229e0593c114b05c8246ee
弄潮者 <497128887@qq.com>
2015-11-17 19:50:48 +0800

    基本背景的搭建

---
+++ NCZ_DBDT/NCZ_DBDT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NCZ_DBDT
+//
+//  Created by 弄潮者 on 15/11/17.
+//  Copyright © 2015年 弄潮者. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NCZ_DBDT/NCZ_DBDTTests/NCZ_DBDTTests.swift
@@ -0,0 +1,36 @@
+//
+//  NCZ_DBDTTests.swift
+//  NCZ_DBDTTests
+//
+//  Created by 弄潮者 on 15/11/17.
+//  Copyright © 2015年 弄潮者. All rights reserved.
+//
+
+import XCTest
+@testable import NCZ_DBDT
+
+class NCZ_DBDTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
